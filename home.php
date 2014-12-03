7f13eda63d0f91de3ad65896f1f88f81939b9127
Teagan Atwater <tatwater@outlook.com>
2014-12-02 21:15:50 -0500

    Update Teagan's Ubuntu version

--- home.php
+++ home.php
@@ -6,6 +6,15 @@
  */
 get_header(); ?>
 
-
+<section class="hero img home">
+  <div class="shade"></div>
+  <div class="content">
+    <div class="navbar">
+      <div class="logo"><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/rocket-white.png" /> Launch</a></div>
+      <nav>hi hi hi hi</nav>
+    </div>
+    <h1>Connecticut College Entrepreneurs.</h1>
+  </div>
+</section>
 
 <?php get_footer(); ?>
