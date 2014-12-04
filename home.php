343bf01f0808ddfaba6d46fc195d1d47f0e75cc1
Teagan Atwater <tatwater@outlook.com>
2014-12-03 22:31:06 -0500

    Improve header, add video placeholder

--- home.php
+++ home.php
@@ -9,9 +9,21 @@ get_header(); ?>
 <section class="hero img home">
   <div class="shade"></div>
   <div class="content">
-    <h1>Conn College Entrepreneurs.</h1>
-    <p>One stop shop for all your entrepreneurship needs.</p>
+    <h1>We're<br />Conn<span>|</span>trepreneurs</h1>
+    <p class="bar"><a class="button right">Learn More ➞</a> Meet a hardworking group of students dedicated to constantly making the world a better place and never ceasing to learn.</p>
   </div>
+</section>
+<section>
+  <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+</section>
+<section class="video">
+  <a><i class="fa fa-play"></i></a>
+  <img style="width: 100%;" src="<?php bloginfo('template_directory'); ?>/img/vidbanner.png" />
+  <!-- <iframe width="560" height="315" src="//www.youtube.com/embed/wT9d-_1BSCA" frameborder="0" allowfullscreen></iframe> -->
+  <!-- <iframe src="//player.vimeo.com/video/77712093" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="http://vimeo.com/77712093">Neuehouse</a> from <a href="http://vimeo.com/neuehouse">NeueHouse</a> on <a href="https://vimeo.com">Vimeo</a>.</p> -->
+</section>
+<section>
+  <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
 </section>
 
 <?php get_footer(); ?>
